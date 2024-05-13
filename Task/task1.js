00d4731e51cbcce14c11c318f0001c1124f32585
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-13 10:47:10 +0530

    task

--- Task/task1.js
+++ Task/task1.js
@@ -1,11 +1,26 @@
-var color= ["red","green","yellow","blue","pink","brown","teal"]
+// // var color= ["red","green","yellow","blue","pink","brown","teal"]
 
+
+// const changeColor = ()=>{
+
+//     var mycolor = document.getElementById("mycolor")
+//     var random = Math.trunc(Math.random()* color.length)
+//     // console.log(random);
+
+//     mycolor.style.backgroundColor = color[random]
+// }
+
+
+var image = ["img1.jpg","img2.jpg","img3.jpeg","img4.avif","img5.jpg"]
 
 const changeColor = ()=>{
 
-    var mycolor = document.getElementById("mycolor")
-    var random = Math.trunc(Math.random()* color.length)
-    // console.log(random);
+    var random = Math.floor(Math.random()* image.length)
+    console.log(random);
+    var myimg = document.getElementById("myimg")
 
-    mycolor.style.backgroundColor = color[random]
+    var path = "../Images/"+image[random]
+
+    myimg.src = path
+
 }
