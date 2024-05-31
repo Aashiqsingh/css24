8e9434e8a6ed18b8ae8df7aca1876b5642ef68ed
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-05-31 10:39:20 +0530

    slideshow

---
+++ Slidesshow/Slideshow.js
@@ -0,0 +1,22 @@
+var images = ["img1.jpg","img2.jpg","img3.jpeg","img4.avif","img5.jpg"]
+
+var currentIndex = 0
+
+var prevbutton = document.getElementById("prev")
+var nextbutton = document.getElementById("next")
+var image = document.getElementById("image")
+
+prevbutton.addEventListener("click",()=>{
+
+    //0 = 0-1+5 % 5
+    // 2 = 2-1+5 % 5
+    currentIndex = (currentIndex-1+images.length)%images.length
+    image.src = "../Images/"+images[currentIndex]
+})
+
+nextbutton.addEventListener("click",()=>{
+    // 
+    // 3 = 3+1 % 5
+    currentIndex = (currentIndex+1)%images.length
+    image.src = "../Images/"+images[currentIndex]
+})
