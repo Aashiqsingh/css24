49c8c3d5551a5c7196523fcb3b8ef700b48e43c0
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-06-05 10:45:03 +0530

    DomNode

--- DomNode/domNode2.js
+++ DomNode/domNode2.js
@@ -4,30 +4,45 @@ const btn = document.getElementById("btn")
 btn.addEventListener("click",()=>{
     const form = document.createElement('form')
     const Nameinput = document.createElement('input')
+    const namelabel = document.createElement('label')
+    namelabel.innerHTML = "Name"
+    form.appendChild(namelabel)
     form.appendChild(Nameinput)
     Nameinput.setAttribute('type','text')
 
 
 
-
+    const emaillabel = document.createElement('label')
+    emaillabel.innerHTML = "Email"
+    form.appendChild(emaillabel)
     const emailinput = document.createElement('input')
     form.appendChild(emailinput)
     emailinput.setAttribute('type','email')
     // emailinput.setAttribute('placeholder','Enter your email')
     emailinput.placeholder = "Enter your email"
 
-    
+
 
     const submit = document.createElement('input')
-    submit.setAttribute('type','submit')
-    submit.setAttribute('value','submit')
+    submit.setAttribute("type","submit")
+    submit.addEventListener("click",(event)=>{
+        event.preventDefault()
+        // submitHandler(event)
+        console.log("form submited..");
+    })
+
+    
+
+
+
+
 
     form.appendChild(submit)
-
-
-
-
-
-
     box.appendChild(form)
 })
+
+// const submitHandler = (e)=>{
+
+//     e.preventDefault();
+//     console.log("Form submitted...");
+// }
---
+++ DomNode/domNode3.js
@@ -0,0 +1,97 @@
+var users = [
+    {
+        id:1,
+        name:"vatsal",
+        age:22,
+        email:"vatsal@gmail.com"
+    },
+    {
+        id:2,
+        name:"vishal",
+        age:25,
+        email:"vishal@gmail.com"
+    },
+    {
+        id:3,
+        name:"raju",
+        age:30,
+        email:"raju@gmail.com"
+    }
+]
+
+
+
+
+// const tbody = document.getElementById("tbody")
+// const btn = document.getElementById('btn')
+// btn.addEventListener("click",()=>{
+//     for(let i=0;i<users.length;i++){
+
+//         const tr = document.createElement('tr')
+//         const Nametd = document.createElement('td')
+//         Nametd.innerHTML = users[i].name
+    
+//         const Agetd = document.createElement('td')
+//         Agetd.innerHTML = users[i].age
+    
+//         const Emailtd = document.createElement('td')
+//         Emailtd.innerHTML = users[i].email
+
+//         const submittd = document.createElement('td')
+//         const btn = document.createElement('button')
+
+//         btn.addEventListener("click",()=>{
+//             alert("name = "+users[i].name)
+//         })
+//         btn.innerHTML = "Details"
+    
+    
+        
+//         tr.appendChild(Nametd)
+//         tr.appendChild(Agetd)
+//         tr.appendChild(Emailtd)
+//         tr.appendChild(submittd)
+//         submittd.appendChild(btn)
+//         tbody.appendChild(tr)
+//     }
+// })
+
+
+const tbody = document.getElementById("tbody")
+const btn = document.getElementById('btn')
+var i = 0;
+var n = 0;
+btn.addEventListener("click",()=>{
+   
+
+
+        const tr = document.createElement('tr')
+        const Nametd = document.createElement('td')
+        Nametd.innerHTML = users[i].name
+    
+        const Agetd = document.createElement('td')
+        Agetd.innerHTML = users[i].age
+    
+        const Emailtd = document.createElement('td')
+        Emailtd.innerHTML = users[i].email
+
+        const submittd = document.createElement('td')
+        const btn = document.createElement('button')
+
+        btn.addEventListener("click",()=>{
+            
+            
+        })
+        btn.innerHTML = "Details"
+    
+    
+        
+        tr.appendChild(Nametd)
+        tr.appendChild(Agetd)
+        tr.appendChild(Emailtd)
+        tr.appendChild(submittd)
+        submittd.appendChild(btn)
+        tbody.appendChild(tr)
+    
+        i++;
+})
