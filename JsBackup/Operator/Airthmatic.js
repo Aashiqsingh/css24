f25e10d42e488f5f6e3a2dc55dcb9ff1e46c1446
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-07-12 15:48:29 +0530

    operator

---
+++ JsBackup/Operator/Airthmatic.js
@@ -0,0 +1,13 @@
+// Airthmatic operator : + , - , * , / , % , ** 
+
+let a = 3;
+let b = 4;
+
+
+// console.log("Addition = ",a+b);
+// console.log("subtraction = ",a-b);
+// console.log("multiplication = ",a*b);
+// console.log("Divide = ",a/b);
+// console.log("module = ",a%b);
+
+// console.log("expo = ",a**b); 
---
+++ JsBackup/Operator/Relational.js
@@ -0,0 +1,27 @@
+// - relational operator : = , == , === , <= , >= , != , !== 
+
+// let a = "12";
+// let b = 12;
+
+
+// console.log("Double iseqaul = ",a == b); // true
+// console.log("not equal iseqaul = ", a != b);
+// console.log("less than is = ",a <= b );
+// console.log("greater than is = ",a>=b);
+
+// console.log("triple iseqaul = ",a === b); // false
+
+// console.log("not double iseqaul = ", a !== b);
+
+var fact = 1;
+for(let i=1;i<=5;i++){
+
+    // fact = fact * i;
+    // fact *= i;
+
+    sum = sum + i;
+
+    sum += i;
+}
+
+console.log(fact);
